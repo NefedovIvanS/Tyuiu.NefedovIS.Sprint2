72f9a9096c62df1d291638503e500e34e3697ad5
NefedovIvanS <Tyuiu.NefedovIS@outlook.com>
2024-10-13 21:55:50 +0500

    Task6

---
+++ Tyuiu.NefedovIS.Sprint2.Task6.V3.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+using Tyuiu.NefedovIS.Sprint2.Task6.V3.Lib;
+
+namespace Tyuiu.NefedovIS.Sprint2.Task6.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.NefedovIS.Sprint2.Task6.V3/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.NefedovIS.Sprint2.Task6.V3.Lib;
+
+namespace Tyuiu.NefedovIS.Sprint2.Task6.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.FindDayName(x));
+            Console.ReadKey();
+        }
+    }
+}
