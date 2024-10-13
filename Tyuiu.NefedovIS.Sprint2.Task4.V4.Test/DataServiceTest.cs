8d539766187499ed9405bb1b1a20041f6fcb4a7b
NefedovIvanS <Tyuiu.NefedovIS@outlook.com>
2024-10-13 15:22:45 +0500

    Task4

---
+++ Tyuiu.NefedovIS.Sprint2.Task4.V4.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+using Tyuiu.NefedovIS.Sprint2.Task4.V4;
+
+namespace Tyuiu.NefedovIS.Sprint2.Task4.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
